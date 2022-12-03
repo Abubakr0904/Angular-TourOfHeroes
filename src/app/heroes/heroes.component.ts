0e5ec0583fa75e84ba830da5d418f8a3338ae5f6
Abubakr Bakhromov <baxromovabubakr@gmail.com>
2022-12-03 22:02:24 +0500

    routing functionality added

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,18 +10,19 @@ import { MessageService } from '../message.service';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
-  selectedHero?: Hero;
-  
+  // selectedHero?: Hero; // obsolete
+
   constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`Heroes component: Selected hero id=${hero.id}`);
-  }
+  // obsolete after adding routing functionality
+  // onSelect(hero: Hero): void {
+  //   this.selectedHero = hero;
+  //   this.messageService.add(`Heroes component: Selected hero id=${hero.id}`);
+  // }
 
   getHeroes(): void {
     this.heroService.getHeroes()
